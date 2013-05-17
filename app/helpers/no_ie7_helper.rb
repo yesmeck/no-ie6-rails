eb973d042aee26e4937f5b616b4359e63a436b78
Meck <yesmeck@gmail.com>
2013-05-17 19:22:31 +0800

    Byebye IE6!

--- app/helpers/no_ie7_helper.rb
+++ app/helpers/no_ie7_helper.rb
@@ -1,5 +1,5 @@
 module NoIe7Helper
   def no_ie7_notice
-    "<!--[if lte IE 6]>#{javascript_include_tag("no_ie7")}<![endif]-->".html_safe
+    "<!--[if lte IE 7]>#{javascript_include_tag("no_ie7")}<![endif]-->".html_safe
   end
 end
